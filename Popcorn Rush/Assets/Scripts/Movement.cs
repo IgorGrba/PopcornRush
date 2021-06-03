ecc5b93a7c30091629925c56cc95dbee447a6574
Logerzdev <69135918+Logerzdev@users.noreply.github.com>
2021-06-03 17:01:50 +0200

    Push

---
+++ Popcorn Rush/Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float controlSpeed;
+
+
+    void Update()
+    {
+
+        transform.position += Vector3.forward * Time.deltaTime * speed;
+        if (Input.touchCount > 0)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            float NewPos = touchDeltaPosition.x * controlSpeed / Screen.width;
+            transform.Translate(NewPos, 0, 0);
+
+        }
+    }
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
