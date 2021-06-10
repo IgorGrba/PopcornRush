8041f541c90567d07f91035747d8c6bfb5642b59
Logerzdev <72897578+LogerzGit@users.noreply.github.com>
2021-06-10 15:18:18 +0200

    push

--- PopcornRush3D/Assets/Scripts/Movement.cs
+++ PopcornRush3D/Assets/Scripts/Movement.cs
@@ -1,56 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] PlayerManager playerManager;
-    [SerializeField] float movementSpeed;
-    [SerializeField] float controlSpeed;
-    [SerializeField] Animator myAnim;
 
-    //Touch Settings
-    [SerializeField] bool isTouching;
-    float touchPosX;
-    Vector3 direction;
-
-    void Start()
-    {
-
-    }
+    public float speed = 5f;
+    public float controlSpeed;
 
 
     void Update()
     {
-        GetInput();
+
+        transform.position += Vector3.forward * Time.deltaTime * speed;
+        if (Input.touchCount > 0)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            float NewPos = touchDeltaPosition.x * controlSpeed / Screen.width;
+            transform.Translate(NewPos, 0, 0);
+
+        }
     }
-
-    private void FixedUpdate() {
-        if(playerManager.playerState==PlayerManager.PlayerState.Move) {
-            transform.position += Vector3.forward * movementSpeed * Time.fixedDeltaTime;
-        }
-
-        ///TRY TO FIGURE IT OUT
-        // if(playerManager.playerState==PlayerManager.PlayerState.Stop)
-        // {
-        //     myAnim.SetBool("isRunning", false);
-        // }
-        
-        
-        if(isTouching) {
-            touchPosX += Input.GetAxis("Mouse X") * controlSpeed *Time.fixedDeltaTime;
-        }
-        
-
-        transform.position = new Vector3(touchPosX, transform.position.y, transform.position.z);
-    }
-
-    void GetInput() {
-        if(Input.GetMouseButton(0)) {
-            isTouching=true;
-        }
-        else {
-            isTouching=false;
-        }
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
     }
 }
